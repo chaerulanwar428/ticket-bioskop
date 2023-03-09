49b2133a71d0d077a29c7670513d553c886e1e68
chaerul anwar <chaerulanwar428@gmail.com>
2023-03-09 21:40:33 +0700

    menambahkan module

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {getMovieList, searchMovie} from './api';
+import { useEffect,useState } from 'react';
+
+const App = () => {
+ 
+  return (
+  );
+}
+
+export default App;
