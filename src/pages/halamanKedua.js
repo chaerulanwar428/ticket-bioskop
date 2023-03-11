bba5cad588ef0c2fe0d3958cd882fa49fac3dc62
chaerul anwar <chaerulanwar428@gmail.com>
2023-03-11 15:33:34 +0700

    mencoba kembali

--- src/pages/halamanKedua.js
+++ src/pages/halamanKedua.js
@@ -1,22 +1,37 @@
 // import List from '../components/List';
-import '../App.css';
-import "../style/landingpage.css";
-import NavigationBar2 from '../components/NavigationBar2';
+// import '../App.css';
+// import "../style/landingpage.css";
+// import NavigationBar2 from '../components/NavigationBar2';
+// import Trending from '../components/Trending';
 import Trending from '../components/Trending';
+import '../App.css';
+import Intro from '../components/Intro';
+import NavigationBar from '../components/NavigationBar';
+import "../style/landingpage.css";
+import Superhero from '../components/Superhero';
 
 
 const HalamanKedua = () => {
  
   return (
-    <>
+    // <div>
+    //   <NavigationBar2/>
+    //   <div className='jadwalbesok'>
+    //     <List/>
+    //   </div>
+    // </div>
     <div>
-      <NavigationBar2/>
-      <div className='jadwalbesok'>
-        {/* <List/> */}
-        <Trending/>
-      </div>
+    <div className="myBG">
+    <NavigationBar/>
+    <Intro/>
     </div>
-    </>
+    <div className="trending" id="trending">
+      <Trending />
+    </div>
+    <div className="superhero" id="superhero">
+      <Superhero/>
+    </div>
+  </div>
   );
 }
 
