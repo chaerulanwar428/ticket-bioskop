927513114accd419bb69770c41bad54e9a32a2a3
chaerul anwar <chaerulanwar428@gmail.com>
2023-03-11 15:12:36 +0700

    merncoba ulang

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<HalamanKesatu/>}/>
-          <Route path="/HalamanKedua" element={<HalamanKedua/>}/>
+          <Route path="/halamanKedua" element={<HalamanKedua/>}/>
         </Routes>
       </Router>
   );
